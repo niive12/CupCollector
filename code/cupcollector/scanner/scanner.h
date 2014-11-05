c11a70f2de7a86dc14117d3998dd6024928a61b0
Mikael <miwes12@student.sdu.dk>
2014-11-05 01:43:44 +0100

    Scanner files updated.

--- code/cupcollector/scanner/scanner.h
+++ code/cupcollector/scanner/scanner.h
@@ -8,51 +8,12 @@
 #pragma once
 
 #include <iostream>
-#include "tekmap/tekmap.hpp"
+#include "../tekmap/tekmap.hpp"
 #include <forward_list>
 
 using namespace std;
 
-
-class scanner
-{
-public:
-    /**
-     * @brief scanner Constructor
-     * @param x0 x-coordinate of center of circle
-     * @param y0 y-coordinate of center of circle
-     * @param r the radius of the circle in pixels
-     *
-     * Will allocate an array with all the coordinate inside the circle with center (x0,y0) and radius r.
-     * Will make coordinate_array point tot the array.
-     */
-    scanner(unsigned int x0, unsigned int y0, int r);
-
-    /**
-     * @brief Scanner function
-     * @param x0 x-coordinate of center of circle
-     * @param y0 y-coordinate of center of circle
-     *
-     * Not finished. Should not be void!
-     */
-    void scan(unsigned int x0, unsigned int y0);
-
-    /**
-     * @brief scanner Destructor
-     *
-     * Deletes the dynamic allocated array
-     */
-    ~scanner();
-
-private:
-    /** @brief coordinate_array will point to an array with all coordinate inside the circle, when the constructor has run */
-    pos_t* coordinate_array = nullptr;
-    //auto coordinate_array = make_shared<tekMap::pos_t>;
-};
-
-
-
-struct cupScanner {
+struct scanner {
     /**
      * @brief scan Returns the number of cups within scanner radius.
      * @param center Center of scanner circle.
