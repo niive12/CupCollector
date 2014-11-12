386dae1422f5aba708297608ea8e51ecb4e3bbd7
Mikael <miwes12@student.sdu.dk>
2014-11-12 11:17:35 +0100

    Norm2 Brushfire map means better path.

--- code/cupcollector/test.hpp
+++ code/cupcollector/test.hpp
@@ -13,6 +13,95 @@
 
 using namespace std;
 
+class norm2BrushfireMap : public tekMap<double> {
+protected:
+	virtual list< pos_t > findObstacleBorders(const pixelshadeMap &img) const
+	{
+		unordered_set<pos_t> resulting_coords;
+		const array<array<int,2>,8> neighbours =
+		{{ {-1,0}, /* W */ {1,0}, /* E */
+		   {0,-1}, /* N */ {0,1}, /* S */
+		   {-1,-1},/* NW */{1,-1}, /* NE */
+		   {1,1}, /* SE */{-1,1} /* SW */
+		 }};
+		for( int x = 0; x < (int)(img.getWidth()); ++x) {
+			for( int y = 0; y < (int)(img.getHeight()); ++y) {
+				if( WSPACE_IS_OBSTACLE( img.const_coordVal(x,y) ) ) {
+					for(auto n : neighbours) {
+						pos_t w = pos_t(x+n.at(0),y+n.at(1));
+						//If neighbour is within image borders:
+						if( img.isInMap(w) ) {
+							//if the neighbour is not an obstacle:
+							if( !WSPACE_IS_OBSTACLE( img.const_coordVal(w) ) ) {
+								resulting_coords.emplace(x,y);
+								break;
+							}
+						}
+					}
+				}
+			}
+		}
+		return move(list<pos_t>(resulting_coords.begin(),resulting_coords.end()));
+	}
+
+	virtual void wave(const pixelshadeMap &img, const list<pos_t> &goals)
+	{
+			const double sqrt2 =1.4142135623730950488016887242097;
+			const array<array<int,2>,8> neighbours =
+			{{ {-1,0}, /* W */ {1,0}, /* E */
+			   {0,-1}, /* N */ {0,1}, /* S */
+			   {-1,-1},/* NW */{1,-1}, /* NE */
+			   {1,1}, /* SE */{-1,1} /* SW */
+			 }};
+			myMap.clear();
+			//4000 looks nicer when printed, but any high value (INF) works:
+			myMap.resize( img.getWidth(), vector<myValType>( img.getHeight() , 4000));
+			//myMp.resize( img->getWidth(), vector<myValType>( img->getHeight() , numeric_limits<myValType>::max()));
+			vector<vector<bool> > visited(img.getWidth(),vector<bool>(img.getHeight(),false));
+			priority_queue<edgeType,deque<edgeType>,edgeComp > q;
+			for(auto i : goals) {
+					(myMap[i.cx()])[i.cy()] = WAVE_VAL_GOAL;
+					q.emplace(i,(myMap[i.cx()])[i.cy()]);
+			}
+			while(!q.empty()) {
+					edgeType cur = q.top();
+					q.pop();
+					visited[cur.first.cx()][cur.first.cy()]=true;
+					for(auto n : neighbours) {
+							pos_t w = cur.first+pos_t(n.at(0),n.at(1));
+							//If neighbour is within image borders:
+							if( img.isInMap(w) ) {
+									//if the neighbour is an unvisited non-obstacle:
+									if( ( !WSPACE_IS_OBSTACLE( img.const_coordVal(w) ) )
+													&& ( !( (visited[w.cx()])[w.cy()] ) )
+													&& ((myMap[w.cx()])[w.cy()] != WAVE_VAL_GOAL)
+													) {
+											if(
+															((myMap[cur.first.cx()])[cur.first.cy()]
+															 +(((n.at(0)==0) || (n.at(1)==0))?1.0:sqrt2))
+															<((myMap[w.cx()])[w.cy()])
+															) {
+													//increment:
+													(myMap[w.cx()])[w.cy()]
+																	= (myMap[cur.first.cx()])[cur.first.cy()]
+																	+(((n.at(0)==0) || (n.at(1)==0))?1.0:sqrt2);
+													//Add it to the wavefront
+													q.emplace(w,(myMap[w.cx()])[w.cy()]);
+											}
+									}
+							}
+					}
+			}
+	}
+
+public:
+	norm2BrushfireMap(const pixelshadeMap &img) {
+		list<pos_t> borders = findObstacleBorders(img);
+		wave(img,borders);
+	}
+
+};
+
 class tester {
 public:
 	static unordered_set<pos_t> getBrushEdges(const brushfireMap &brush, size_t radius)
@@ -24,6 +113,21 @@ public:
 				{
 					result.emplace(x,y);
 				}
+			}
+		}
+		return move(result);
+	}
+
+	static unordered_set<pos_t> getBrushEdges(const unordered_set<pos_t> &freespace, const norm2BrushfireMap &brush, size_t radius)
+	{
+		unordered_set<pos_t> result;
+		for(coordIndexType x = 0; x<(coordIndexType)brush.getWidth(); ++x) {
+			for(coordIndexType y = 0; y<(coordIndexType)brush.getHeight(); ++y) {
+				if(freespace.find(pos_t(x,y))!=freespace.end())
+					if((((size_t)(brush.const_coordVal(x,y)-1))%(2*radius))==radius)
+					{
+						result.emplace(x,y);
+					}
 			}
 		}
 		return move(result);
@@ -67,6 +171,12 @@ public:
 		for(coordIndexType x=0; x<(coordIndexType)imgToPad.getWidth(); ++x)
 			for(coordIndexType y=0; y<(coordIndexType)imgToPad.getHeight(); ++y)
 				if(brush.const_coordVal(x,y) <= (brushfireMap::myValType)(padValue))
+					imgToPad.coordVal(x,y) = WSPACE_OBSTACLE;
+	}
+	static void pad(pixelshadeMap &imgToPad, const norm2BrushfireMap &brush, const size_t padValue) {
+		for(coordIndexType x=0; x<(coordIndexType)imgToPad.getWidth(); ++x)
+			for(coordIndexType y=0; y<(coordIndexType)imgToPad.getHeight(); ++y)
+				if(brush.const_coordVal(x,y) <= (norm2BrushfireMap::myValType)(padValue))
 					imgToPad.coordVal(x,y) = WSPACE_OBSTACLE;
 	}
 
@@ -170,6 +280,8 @@ public:
 
 	static void getRemainders(const pixelshadeMap &img, unordered_set<pos_t> &coordSet, const pos_t &freespaceCoord) {
 		unordered_set<pos_t> freespace = img.findFreespace(freespaceCoord);
+		//Sorting freespace for robot motion is not as easy as set<pos_t>.
+		//set<pos_t> sorted(freespace.begin(),freespace.end());
 		for(auto c:freespace)
 			if(isRemainder(c,coordSet,ROBOT_DYNAMICS_RADIUS))
 				coordSet.insert(c);
@@ -179,9 +291,21 @@ public:
 	{
 		const pos_t start = {ROBOT_START_X,ROBOT_START_Y};
 		pixelshadeMap theImg(img), configurationSpace(img);
-		brushfireMap brush(theImg,start);
+
+
+		//brushfireMap brush(theImg,start);
+		norm2BrushfireMap brush(theImg);
+
+
 		pad(configurationSpace,brush,ROBOT_DYNAMICS_RADIUS);
-		unordered_set<pos_t> coords = getBrushEdges(brush,ROBOT_DYNAMICS_RADIUS);
+
+		//unordered_set<pos_t> coords = getBrushEdges(brush,ROBOT_DYNAMICS_RADIUS);
+		unordered_set<pos_t> free = theImg.findFreespace(start);
+		unordered_set<pos_t> coords = getBrushEdges(free,brush,ROBOT_DYNAMICS_RADIUS);
+
+
+
+
 
 		if(banim) {
 			cout << "Saving configuration space as \"test_configuration_space.pgm\"... " << flush;
@@ -275,9 +399,12 @@ public:
 		cout << "\r                                                                " << endl;
 		if(banim) {
 			cout << "Saving robot path as \"test_robot_path_IDX.pgm\"... " << flush;
+			ostringstream anim;
+			anim << "test_robot_path_full_" << setw(6) << setfill('0')
+				 << robotPath.size() << "_" << n << ".pgm";
 			for(auto v:robotPath)
 				img->setPixel8U(v.cx(),v.cy(),20);
-			img->saveAsPGM("test_robot_path_full.pgm");
+			img->saveAsPGM(anim.str());
 			theImg.shade(img);
 			cout << "Done." << endl;
 		}
